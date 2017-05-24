13bdbe7f9ef3e055f7c9e3a1849870beb655d0fc
olol85 <olol85@gmail.com>
2017-05-24 16:36:05 +0200

    conf and emacs

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -491,17 +491,6 @@ Repeated invocations toggle between the two most recently open buffers."
   (setq whitespace-style '(face tabs spaces newline empty
                                 trailing tab-mark newline-mark)))
 
-(use-package whitespace-cleanup-mode
-  :diminish whitespace-cleanup-mode
-  :init (global-whitespace-cleanup-mode))
-
-;; yasnippet
-(use-package yasnippet
-  :commands (yas-expand yas-insert-snippet)
-  :config
-  (use-package java-snippets)
-(add-to-list 'c-default-style '(csharp-mode . "c#"))
-
 (defun work-style ()
   (interactive)
   (ggtags-mode)
@@ -600,8 +589,6 @@ Repeated invocations toggle between the two most recently open buffers."
 
 (menu-bar-mode -1)
 
-
-
 (use-package git-timemachine
   :defer t)
 
@@ -675,7 +662,7 @@ Repeated invocations toggle between the two most recently open buffers."
     ("721bb3cb432bb6be7c58be27d583814e9c56806c06b4077797074b009f322509" "3eb93cd9a0da0f3e86b5d932ac0e3b5f0f50de7a0b805d4eb1f67782e9eb67a4" "73a13a70fd111a6cd47f3d4be2260b1e4b717dbf635a9caee6442c949fad41cd" "003a9aa9e4acb50001a006cfde61a6c3012d373c4763b48ceb9d523ceba66829" "b59d7adea7873d58160d368d42828e7ac670340f11f36f67fa8071dbf957236a" default)))
  '(package-selected-packages
    (quote
-    (linum-relative window-purpose dedicated latex-mode latex tex git-timemachine relative-line-numbers airline-themes ranger ac-anaconda company-anaconda anaconda-mode elpy evil-magit znc yaml-mode whitespace-cleanup-mode which-key virtualenvwrapper use-package unfill try toml-mode ssh-config-mode smooth-scroll sly scratch savekill rust-mode regex-tool puppet-mode processing-mode powershell powerline popwin pkgbuild-mode org-plus-contrib nginx-mode matlab-mode markdown-mode magit less-css-mode ledger-mode java-snippets helm-projectile helm-gitignore helm-descbinds helm-company helm-ag haskell-mode handlebars-mode gnuplot gitconfig-mode ggtags general fortune-cookie flycheck f evil-visualstar evil-surround evil-snipe evil-smartparens evil-matchit evil-escape evil-easymotion evil-commentary evil-anzu electric-spacing dtrt-indent dockerfile-mode docker demangle-mode csharp-mode crontab-mode company-c-headers color-theme-solarized cmake-mode bison-mode auto-compile ag adaptive-wrap ace-window spaceline pyenv-mode)))
+    (linum-relative window-purpose dedicated latex-mode latex tex git-timemachine airline-themes ranger ac-anaconda company-anaconda anaconda-mode elpy evil-magit znc yaml-mode whitespace-cleanup-mode which-key virtualenvwrapper use-package unfill try toml-mode ssh-config-mode smooth-scroll sly scratch savekill rust-mode regex-tool puppet-mode processing-mode powershell powerline popwin pkgbuild-mode org-plus-contrib nginx-mode matlab-mode markdown-mode magit less-css-mode ledger-mode java-snippets helm-projectile helm-gitignore helm-descbinds helm-company helm-ag haskell-mode handlebars-mode gnuplot gitconfig-mode ggtags general fortune-cookie flycheck f evil-visualstar evil-surround evil-snipe evil-smartparens evil-matchit evil-escape evil-easymotion evil-commentary evil-anzu electric-spacing dtrt-indent dockerfile-mode docker demangle-mode csharp-mode crontab-mode company-c-headers color-theme-solarized cmake-mode bison-mode auto-compile ag adaptive-wrap ace-window spaceline pyenv-mode)))
  '(split-height-threshold nil)
  '(split-width-threshold 0))
 (custom-set-faces
