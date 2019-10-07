55989d50daef2bf765ac71a69aec95bc5d8fe2ea
olol85 <olol85@gmail.com>
2019-10-07 13:36:22 +0200

    polybar

---
+++ config/polybar/polybar.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+# Terminate already running bar instances
+
+killall -q polybar
+
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+
+polybar -r  example &
--- lsyncd/lsyncd.lua
+++ lsyncd/lsyncd.lua
@@ -8,9 +8,10 @@ settings {
 sync {
         default.rsyncssh,
         source = "/home/krakapwa/Documents/software",   
-        host="laurent@130.92.124.90",
-        targetdir = "/home/laurent.lejeune/Documents/software",
-        delete = "running",
-        delay = 3,
-
-    }
+        host = "lejeune@submit.unibe.ch",
+        targetdir = "/home/ubelix/artorg/lejeune/Documents/software",
+        delay=1,
+        rsync = {
+                rsh = "/usr/bin/ssh -l lejeune -i /home/krakapwa/.ssh/id_rsa -o StrictHostKeyChecking=no"
+        }
+}
