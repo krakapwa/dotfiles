b31915d251890b97517179e539811c8662b68994
olol85 <olol85@gmail.com>
2019-10-15 15:43:10 +0200

    polybar

--- lsyncd/lsyncd.lua
+++ lsyncd/lsyncd.lua
@@ -15,3 +15,15 @@ sync {
                 rsh = "/usr/bin/ssh -l lejeune -i /home/krakapwa/.ssh/id_rsa -o StrictHostKeyChecking=no"
         }
 }
+
+
+--sync {
+--        default.rsyncssh,
+--        source = "/home/krakapwa/Documents/software",   
+--        host = "laurent@130.92.124.90",
+--        targetdir = "/home/laurent/Documents/software",
+--        delay=1,
+--        rsync = {
+--                rsh = "/usr/bin/ssh -l laurent -i /home/krakapwa/.ssh/id_rsa -o StrictHostKeyChecking=no"
+--        }
+--}
